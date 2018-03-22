f00b0ab5e233988411fe78b643fa9c7344fcecb3
Benjamin Oluwalowo <olubeno@dunwoody.edu>
2018-03-22 14:03:31 -0500

    very close

--- Project3/Form1.Designer.cs
+++ Project3/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.idLabel = new System.Windows.Forms.Label();
             this.idText = new System.Windows.Forms.TextBox();
             this.newResTab = new System.Windows.Forms.TabPage();
+            this.returnButton = new System.Windows.Forms.Button();
             this.goButton = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.athlete = new System.Windows.Forms.RadioButton();
@@ -55,13 +56,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tabs = new System.Windows.Forms.TabControl();
             this.scholarshipTab = new System.Windows.Forms.TabPage();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.athleteTab = new System.Windows.Forms.TabPage();
             this.workerTab = new System.Windows.Forms.TabPage();
-            this.label7 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.button1 = new System.Windows.Forms.Button();
+            this.return2 = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
             this.resSearchTab.SuspendLayout();
             this.newResTab.SuspendLayout();
             this.selectionTab.SuspendLayout();
@@ -107,12 +109,13 @@
             // resSearchTab
             // 
             this.resSearchTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("resSearchTab.BackgroundImage")));
+            this.resSearchTab.Controls.Add(this.return2);
             this.resSearchTab.Controls.Add(this.idLabel);
             this.resSearchTab.Controls.Add(this.idText);
             this.resSearchTab.Location = new System.Drawing.Point(4, 22);
-            this.resSearchTab.Margin = new System.Windows.Forms.Padding(2);
+            this.resSearchTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.resSearchTab.Name = "resSearchTab";
-            this.resSearchTab.Padding = new System.Windows.Forms.Padding(2);
+            this.resSearchTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.resSearchTab.Size = new System.Drawing.Size(706, 431);
             this.resSearchTab.TabIndex = 3;
             this.resSearchTab.Text = "Residence Search";
@@ -140,20 +143,31 @@
             // newResTab
             // 
             this.newResTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("newResTab.BackgroundImage")));
-            this.newResTab.Controls.Add(this.button1);
+            this.newResTab.Controls.Add(this.returnButton);
             this.newResTab.Controls.Add(this.goButton);
             this.newResTab.Controls.Add(this.label4);
             this.newResTab.Controls.Add(this.athlete);
             this.newResTab.Controls.Add(this.worker);
             this.newResTab.Controls.Add(this.scholarship);
             this.newResTab.Location = new System.Drawing.Point(4, 22);
-            this.newResTab.Margin = new System.Windows.Forms.Padding(2);
+            this.newResTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.newResTab.Name = "newResTab";
-            this.newResTab.Padding = new System.Windows.Forms.Padding(2);
+            this.newResTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.newResTab.Size = new System.Drawing.Size(706, 431);
             this.newResTab.TabIndex = 2;
             this.newResTab.Text = "New Residence";
             this.newResTab.UseVisualStyleBackColor = true;
+            // 
+            // returnButton
+            // 
+            this.returnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.returnButton.Location = new System.Drawing.Point(479, 370);
+            this.returnButton.Name = "returnButton";
+            this.returnButton.Size = new System.Drawing.Size(196, 43);
+            this.returnButton.TabIndex = 5;
+            this.returnButton.Text = "Return to Selection";
+            this.returnButton.UseVisualStyleBackColor = true;
+            this.returnButton.Click += new System.EventHandler(this.returnButton_Click);
             // 
             // goButton
             // 
@@ -216,9 +230,9 @@
             this.selectionTab.Controls.Add(this.findRes);
             this.selectionTab.Controls.Add(this.newRes);
             this.selectionTab.Location = new System.Drawing.Point(4, 22);
-            this.selectionTab.Margin = new System.Windows.Forms.Padding(2);
+            this.selectionTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.selectionTab.Name = "selectionTab";
-            this.selectionTab.Padding = new System.Windows.Forms.Padding(2);
+            this.selectionTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.selectionTab.Size = new System.Drawing.Size(706, 431);
             this.selectionTab.TabIndex = 1;
             this.selectionTab.Text = "Selection";
@@ -259,7 +273,6 @@
             // menuTab
             // 
             this.menuTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("menuTab.BackgroundImage")));
-            this.menuTab.Controls.Add(this.welcome);
             this.menuTab.Controls.Add(this.login);
             this.menuTab.Controls.Add(this.titleLabel);
             this.menuTab.Controls.Add(this.password);
@@ -268,9 +281,9 @@
             this.menuTab.Controls.Add(this.label2);
             this.menuTab.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuTab.Location = new System.Drawing.Point(4, 22);
-            this.menuTab.Margin = new System.Windows.Forms.Padding(2);
+            this.menuTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.menuTab.Name = "menuTab";
-            this.menuTab.Padding = new System.Windows.Forms.Padding(2);
+            this.menuTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.menuTab.Size = new System.Drawing.Size(706, 431);
             this.menuTab.TabIndex = 0;
             this.menuTab.Text = "Main Menu";
@@ -279,11 +292,12 @@
             // welcome
             // 
             this.welcome.AutoSize = true;
-            this.welcome.Location = new System.Drawing.Point(309, 123);
+            this.welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcome.Location = new System.Drawing.Point(18, 145);
             this.welcome.Name = "welcome";
-            this.welcome.Size = new System.Drawing.Size(183, 16);
+            this.welcome.Size = new System.Drawing.Size(133, 32);
             this.welcome.TabIndex = 6;
-            this.welcome.Text = "Welcome Resident Director!";
+            this.welcome.Text = "Welcome \r\nResident Director!";
             this.welcome.Visible = false;
             // 
             // login
@@ -311,7 +325,7 @@
             // 
             this.password.Enabled = false;
             this.password.Location = new System.Drawing.Point(370, 202);
-            this.password.Margin = new System.Windows.Forms.Padding(2);
+            this.password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(76, 23);
             this.password.TabIndex = 0;
@@ -321,7 +335,7 @@
             // username
             // 
             this.username.Location = new System.Drawing.Point(370, 169);
-            this.username.Margin = new System.Windows.Forms.Padding(2);
+            this.username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(76, 23);
             this.username.TabIndex = 0;
@@ -358,7 +372,7 @@
             this.tabs.Controls.Add(this.athleteTab);
             this.tabs.Controls.Add(this.workerTab);
             this.tabs.Location = new System.Drawing.Point(192, 11);
-            this.tabs.Margin = new System.Windows.Forms.Padding(2);
+            this.tabs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
             this.tabs.Size = new System.Drawing.Size(714, 457);
@@ -374,11 +388,45 @@
             this.scholarshipTab.Controls.Add(this.label6);
             this.scholarshipTab.Location = new System.Drawing.Point(4, 22);
             this.scholarshipTab.Name = "scholarshipTab";
-            this.scholarshipTab.Padding = new System.Windows.Forms.Padding(3);
+            this.scholarshipTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.scholarshipTab.Size = new System.Drawing.Size(706, 431);
             this.scholarshipTab.TabIndex = 4;
             this.scholarshipTab.Text = "New Scholarship";
             this.scholarshipTab.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(322, 158);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(64, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Floor 8";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(322, 119);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Floor 7";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(126, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Please select a floor:";
             // 
             // label6
             // 
@@ -395,7 +443,7 @@
             this.athleteTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("athleteTab.BackgroundImage")));
             this.athleteTab.Location = new System.Drawing.Point(4, 22);
             this.athleteTab.Name = "athleteTab";
-            this.athleteTab.Padding = new System.Windows.Forms.Padding(3);
+            this.athleteTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.athleteTab.Size = new System.Drawing.Size(706, 431);
             this.athleteTab.TabIndex = 5;
             this.athleteTab.Text = "New Student Athlete";
@@ -406,55 +454,34 @@
             this.workerTab.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("workerTab.BackgroundImage")));
             this.workerTab.Location = new System.Drawing.Point(4, 22);
             this.workerTab.Name = "workerTab";
-            this.workerTab.Padding = new System.Windows.Forms.Padding(3);
+            this.workerTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.workerTab.Size = new System.Drawing.Size(706, 431);
             this.workerTab.TabIndex = 6;
             this.workerTab.Text = "New Student Worker";
             this.workerTab.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // return2
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(126, 119);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(155, 16);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "Please select a floor:";
+            this.return2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.return2.Location = new System.Drawing.Point(486, 362);
+            this.return2.Name = "return2";
+            this.return2.Size = new System.Drawing.Size(196, 43);
+            this.return2.TabIndex = 6;
+            this.return2.Text = "Return to Selection";
+            this.return2.UseVisualStyleBackColor = true;
+            this.return2.Click += new System.EventHandler(this.return2_Click);
             // 
-            // radioButton1
+            // logoutButton
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(322, 119);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(64, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Floor 7";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(322, 158);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(64, 17);
-            this.radioButton3.TabIndex = 4;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Floor 8";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(479, 370);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(196, 43);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Return to Selection";
-            this.button1.UseVisualStyleBackColor = true;
+            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.Location = new System.Drawing.Point(48, 441);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.TabIndex = 7;
+            this.logoutButton.Text = "Logout";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Visible = false;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
             // 
             // mainWindow
             // 
@@ -463,11 +490,13 @@
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(917, 477);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.welcome);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dunLab2);
             this.Controls.Add(this.dunLabel1);
             this.Controls.Add(this.tabs);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "mainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dunwoody Residence Hall";
@@ -518,10 +547,12 @@
         private System.Windows.Forms.Button login;
         private System.Windows.Forms.Label welcome;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button returnButton;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button return2;
+        private System.Windows.Forms.Button logoutButton;
     }
 }
 
--- Project3/Form1.cs
+++ Project3/Form1.cs
@@ -46,45 +46,77 @@ namespace Project3
             }
         }
 
-        ///called when the button is clicked
+        /// <summary>
+        /// called when the button is clicked
+        /// this is coming from main screen to the selection tab
+        /// </summary>
         private void login_Click(object sender, EventArgs e)
         {
             tabs.TabPages.Add(selectionTab);
+            tabs.SelectedIndex = 1;
             welcome.Visible = true;
+            logoutButton.Visible = true;
         }
-
+        /// <summary>
+        /// return button goes to the selection tab from new resident
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            tabs.TabPages.RemoveAt(tabs.SelectedIndex);
+            tabs.SelectedIndex = 1;
+        }
 
         //Clicking new Resident button or Find Resident button
         private void newRes_Click(object sender, EventArgs e)
         {
             tabs.TabPages.Add(newResTab);
-            //newResTab.Click = true;     
+            tabs.SelectedIndex = 2;
         }
         
+        private void findRes_Click(object sender, EventArgs e)
+        {
+            tabs.TabPages.Add(resSearchTab);
+            tabs.SelectedIndex = 2;
+        }
         // Options for when choosing the kind of student being added to the residence
         private void go_Click(object sender, EventArgs e)
         {
             if (scholarship.Checked == true)
             {
                 tabs.TabPages.Add(scholarshipTab);
-
+                
             }
             else if (athlete.Checked == true)
             {
                 tabs.TabPages.Add(athleteTab);
+                
             }
             else if (worker.Checked == true)
             {
                 tabs.TabPages.Add(workerTab);
+                
             }
+
+            tabs.SelectedIndex = 3;
         }
 
-
-        private void findRes_Click(object sender, EventArgs e)
+        private void return2_Click(object sender, EventArgs e)
         {
-            tabs.TabPages.Add(resSearchTab);
+            tabs.TabPages.RemoveAt(tabs.SelectedIndex);
+            tabs.SelectedIndex = 1;
         }
 
-
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            tabs.SelectedIndex = 0;
+            tabs.TabPages.Remove(selectionTab);
+            tabs.TabPages.Remove(newResTab);
+            tabs.TabPages.Remove(resSearchTab);
+            tabs.TabPages.Remove(scholarshipTab);
+            tabs.TabPages.Remove(athleteTab);
+            tabs.TabPages.Remove(workerTab);
+        }
     }
 }
